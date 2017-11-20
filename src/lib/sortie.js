f71a4a53531ede5a955b8a6dc27bfab9c286d6e1
sean2525 <madness48596@gmail.com>
2017-11-20 17:55:43 +0800

    Fixed midnight and raigeki battle api

--- src/lib/sortie.js
+++ src/lib/sortie.js
@@ -40,11 +40,15 @@ class sortie{
 			// fydam: attack enemys damage || fdam: our fleet onDamage || eydam: attack our fleet damage || edam: enemys onDamage
 			// compute Raigeki damage
 			const onDamageRaigeki = ({ api_fydam , api_fdam , api_eydam , api_edam}) => {
-				for(let i = 1 ; i < api_fydam.length; i++){
-					api_f_onDamageHps[i-1] -= Math.round(api_fdam[i])
-					api_f_countDamage[i-1] += Math.round(api_fydam[i])
-					api_e_onDamageHps[i-1] -= Math.round(api_edam[i])
-					api_e_countDamage[i-1] += Math.round(api_eydam[i])
+				for(let i = 0 ; i < api_fydam.length; i++){
+					if(api_f_onDamageHps[i] != undefined){
+						api_f_onDamageHps[i] -= Math.round(api_fdam[i])
+						api_f_countDamage[i] += Math.round(api_fydam[i])
+					}
+					if(api_e_onDamageHps[i] != undefined){
+						api_e_onDamageHps[i] -= Math.round(api_edam[i])
+						api_e_countDamage[i] += Math.round(api_eydam[i])
+					}
 				}
 			}
 			// hougeki
@@ -72,10 +76,10 @@ class sortie{
 					}))
 				}
 			}
-			let nowenemy = []
+			let nowEnemy = []
 			for(let i = 0; i < api_data.api_ship_ke.length; i++){
 				if(api_data.api_ship_ke[i] != -1 && api_data.api_ship_ke[i] !=0 ){
-					nowenemy.push(Object.assign({},{ 
+					nowEnemy.push(Object.assign({},{ 
 						'api_name': store.getters.find_mst_ship(api_data.api_ship_ke[i]).api_name,
 						'api_maxhp': api_e_maxhps[i],
 						'api_nowhp': api_e_onDamageHps[i],
@@ -87,26 +91,33 @@ class sortie{
 			}
 			let battleresult = {
 				'fleet': nowFleet,
-				'enemy': nowenemy
+				'enemy': nowEnemy
 			}
 			store.commit('UPDATE_BATTLE', battleresult)
 		}
 		this.midnight = ({ api_data }) => { // for normal midnight battle
 			if(Object.keys(store.state.api.battleresult).length){
-				let { api_maxhps, api_nowhps } = api_data
-				let api_onDamageHps = [...api_nowhps]
-				let api_countDamage = Array(api_maxhps.length).fill(0)
+				let { api_e_maxhps , api_e_nowhps , api_f_maxhps , api_f_nowhps } = api_data
+				let api_e_onDamageHps = [...api_e_nowhps]
+				let api_f_onDamageHps = [...api_f_nowhps]
+				let api_e_countDamage = Array(api_e_maxhps.length).fill(0)
+				let api_f_countDamage = Array(api_f_maxhps.length).fill(0)
 				let battleresult = store.state.api.battleresult
-				// hougeki
-				const onDamageHougeki = ({ api_at_list , api_df_list , api_damage }) => {
+				// compute Hougeki damage
+				const onDamageHougeki = ({ api_at_eflag, api_at_list, api_df_list, api_damage }) => {
 					let hougeki  = []
-					for(let i = 1 ; i < api_at_list.length ; i++){
-						hougeki.push({'attacker' : api_at_list[i] , 'defender': api_df_list[i] , 'damage': api_damage[i] })
+					for(let i = 0 ; i < api_at_list.length ; i++){
+						hougeki.push({ 'eflag': api_at_eflag[i] ,'attacker': api_at_list[i] , 'defender': api_df_list[i] , 'damage': api_damage[i] })
 					}
 					hougeki.map( kogeki => {
 						for(let x = 0 ; x < kogeki.defender.length ; x++){
-							api_onDamageHps[kogeki.defender[x]] -= Math.round(kogeki.damage[x])
-							api_countDamage[kogeki.attacker] += Math.round(kogeki.damage[x])
+							if(kogeki.eflag){  // 0 => f , 1 => enemys attack
+								api_f_onDamageHps[kogeki.defender[x]] -= Math.round(kogeki.damage[x])
+								api_e_countDamage[kogeki.attacker] += Math.round(kogeki.damage[x])
+							} else {
+								api_e_onDamageHps[kogeki.defender[x]] -= Math.round(kogeki.damage[x])
+								api_f_countDamage[kogeki.attacker] += Math.round(kogeki.damage[x])
+							}
 						}
 					})
 				}
@@ -116,19 +127,34 @@ class sortie{
 				for(let i = 0 ; i < fleet.length ; i++){
 					if(fleet[i] != undefined){
 						nowFleet.push(Object.assign({}, fleet[i], { 
-							'api_nowhp': api_onDamageHps[i+1],
-							'api_damage': fleet[i].api_damage + api_countDamage[i+1]
+							'api_nowhp': api_f_onDamageHps[i],
+							'api_damage': fleet[i].api_damage + api_f_countDamage[i]
 						}))
 					}
 				}
 				let enemy = battleresult.enemy
 				let nowEnemy = []
-				for(let i = 0; i < enemy.length; i++){
-					if(enemy[i] != undefined){
-						nowEnemy.push(Object.assign({}, enemy[i],{ 
-							'api_nowhp': api_onDamageHps[7+i],
-							'api_damage': enemy[i].api_damage + api_countDamage[7+i],
-						}))
+				if(enemy){
+					for(let i = 0; i < enemy.length; i++){
+						if(enemy[i] != undefined){
+							nowEnemy.push(Object.assign({}, enemy[i],{ 
+								'api_nowhp': api_e_onDamageHps[i],
+								'api_damage': enemy[i].api_damage + api_e_countDamage[i],
+							}))
+						}
+					}
+				}else{
+					for(let i = 0; i < api_data.api_ship_ke.length; i++){
+						if(api_data.api_ship_ke[i] != -1 && api_data.api_ship_ke[i] !=0 ){
+							nowEnemy.push(Object.assign({},{
+								'api_name': store.getters.find_mst_ship(api_data.api_ship_ke[i]).api_name,
+								'api_maxhp': api_e_maxhps[i],
+								'api_nowhp': api_e_onDamageHps[i],
+								'api_originhp': api_e_nowhps[i],
+								'api_damage': api_e_countDamage[i],
+								'api_lv': api_data.api_ship_lv[i]
+							}))
+						}
 					}
 				}
 				let midnightbattleresult = {
